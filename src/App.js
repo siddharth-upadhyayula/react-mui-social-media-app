8ea24b247a971baf9b5d39b20a9513d6ae3550c1
sid_upadhyayula <s.upadhyayula1@ufl.edu>
2022-08-09 02:42:49 -0400

    Added dark mode functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import Feed from './components/Feed';
-import Rightbar from './components/Rightbar';
-import Sidebar from './components/Sidebar';
-import Navbar from './components/Navbar';
-import './App.css';
-import { Box } from '@mui/system';
-import { Stack } from '@mui/material';
+
+
+import Sidebar from "./components/Sidebar";
+import Feed from "./components/Feed";
+import Rightbar from "./components/Rightbar";
+import { Box, createTheme, Stack, ThemeProvider } from "@mui/material";
+import Navbar from "./components/Navbar";
+import Add from "./components/Add";
+import { useState } from "react";
 
 function App() {
+  const [mode, setMode] = useState("light");
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
   return (
-    <div className="App">
-      <Navbar />
-      <Box>
-        <Stack direction='row' spacing={2} justifyContent="space-between">
-          <Sidebar />
+    <ThemeProvider theme={darkTheme}>
+      <Box bgcolor={"background.default"} color={"text.primary"}>
+        <Navbar />
+        <Stack direction="row" spacing={2} justifyContent="space-between">
+        <Sidebar setMode={setMode} mode={mode}/>
           <Feed />
           <Rightbar />
         </Stack>
-        
+        <Add />
       </Box>
-      
-    </div>
+    </ThemeProvider>
   );
 }
 
